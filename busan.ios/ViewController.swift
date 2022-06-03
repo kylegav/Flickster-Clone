0cb798476f230af8adbd59c995ddd18e6f43bbb9
kylegav <60828879+kylegav@users.noreply.github.com>
2022-06-02 23:23:48 -0400

    Added basic table view, added delegate and table source controllers.

--- busan.ios/ViewController.swift
+++ busan.ios/ViewController.swift
@@ -7,7 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        cell.textLabel!.text = "row: \(indexPath.row)"
+        
+        return cell
+    }
+    
     
     
     @IBOutlet weak var tableView: UITableView!
@@ -18,6 +35,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
